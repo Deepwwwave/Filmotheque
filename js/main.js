5e9caf2e5ac4fd8751159cec71284cd3128ff3af
deepwwwave <deepwwwave@gmail.com>
2022-04-03 02:31:08 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const API_KEY = "4802f33879989cab6fb0f507fd3939c0";
-let movieResearchByUser
 const API_URL_MOVIES_WEEK = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=fr`;
-const API_URL_SEARCH_MOVIE = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieResearchByUser}&language=fr`
-
+const urlImg = "https://image.tmdb.org/t/p/w500/";
+const urlImgSelectedMovie = "https://image.tmdb.org/t/p/w500"
+let movieResearchByUser;
+let API_URL_RESEARCH_MOVIE;
+let idMovieSelectedByUser;
+let API_URL_MOVIE_SELECTED;
 
 function ajaxRequest(url, functionDisplay) {
   // requete ajax -> asynchrone
@@ -10,7 +13,22 @@ function ajaxRequest(url, functionDisplay) {
     // si response
     .done((res) => {
       functionDisplay(res);
-      console.log(res)
+      console.log(res);
+    })
+    // si erreur
+    .fail((error) => {
+      console.log(error);
+    });
+}
+
+// TEST
+function ajaxRequestTest() {
+  // requete ajax -> asynchrone
+  $.getJSON(`https://api.themoviedb.org/3/movie/9280?api_key=${API_KEY}`)
+    // si response
+    .done((res) => {
+      // functionDisplay(res);
+      console.log(res);
     })
     // si erreur
     .fail((error) => {
@@ -21,11 +39,11 @@ function ajaxRequest(url, functionDisplay) {
 //Une fois le DOM monté => Appel de la fonction de requête ajax
 $(() => {
   ajaxRequest(API_URL_MOVIES_WEEK, displayWeekMovies);
+  ajaxRequestTest();
 });
 
 //fonction qui va créer la liste de films de la semaine à partir de la réponse de la requête ajax,
 function displayWeekMovies(items) {
-  const urlImg = "https://image.tmdb.org/t/p/w500/";
   let dateDeSortie;
   let titre;
   let html = '<h2>Les Films de la semaine</h2><section id="liste_films">';
@@ -40,7 +58,7 @@ function displayWeekMovies(items) {
             <div class="container_film">
                 <h3>${titre}</h3>
                 <ul>
-                    <img src=${urlImg}${items.results[i].poster_path}>
+                    <img class="img_film_semaine" src=${urlImg}${items.results[i].poster_path}>
                     <li>Note : ${items.results[i].vote_average}</li><br/>
                     <li>Nombre de votes : ${items.results[i].vote_count}</li><br/>
                     <li>Date : ${dateDeSortie}</li><br/>
@@ -54,16 +72,13 @@ function displayWeekMovies(items) {
 }
 
 //function qui change la valeur 'movieResearchByUser'
-changeValue=()=> {
-  movieResearchByUser = $("#film").val();
-  console.log(movieResearchByUser);
-}
-
-
+// function changeValue() {
+//   console.log(movieResearchByUser);
+// }
 
 // fonction qui va créer la liste de films de la recherche à partir de la réponse de la requête ajax,
 function displayResearchMovie(items) {
-  let htmlq = '<ul id="liste_film_recherche">';
+  let html = '<ul id="liste_film_recherche">';
   for (let i = 0; i < items.results.length; i++) {
     items.results[i].title !== undefined
       ? (titre = items.results[i].title)
@@ -71,24 +86,67 @@ function displayResearchMovie(items) {
     items.results[i].release_date !== undefined
       ? (dateDeSortie = items.results[i].release_date)
       : (dateDeSortie = "indisponible");
-    htmlq += `
-             <li>${titre}</li>     
+    html += `
+             <li class="titre_film" id=${items.results[i].id}>${titre}</li>     
             `;
   }
-  htmlq += "</ul>";
-  $("#propositions_recherche").html(htmlq);
+  html += "</ul>";
+  $("#propositions_recherche").html(html);
   console.log(items.results);
+
+  $(".titre_film").on("click", eventClickSelectedMovie)
+  
 }
-
-
-
-
-
-
-// Evenement au clic du bouton recherche
-
 
 
 // $("#bouton_search").on("click",  changeValue);
 // $("#bouton_search").on("click",  console.log(API_URL_SEARCH_MOVIE));
-// $("#bouton_search").on("click",  ajaxRequest(API_URL_SEARCH_MOVIE, displayResearchMovie));
+
+function displaySelectedMovie(items) {
+  let html = "<ul class='liste_film_choisit'>";
+  items.release_date !== undefined
+  ? (dateDeSortie = items.release_date)
+  : (dateDeSortie = "indisponible");
+  html += `
+  <div class="titre_image_selected_film">
+  <li>${titre}</li>
+  <li><img class="img_movie_selected" src=${urlImgSelectedMovie}${items.poster_path}></li><br/>
+  </div>
+  <div class="descritpti_film_choisit"><li>Date : ${dateDeSortie}</li><br/>
+  <p>${items.overview}<p><br/>
+  <li>Note : ${items.vote_average}</li><br/>
+  <li>Nombre de votes : ${items.vote_count}</li><br/>
+  <li>compagnies : ${items.production_companies.name}</li><br/>
+  </ul>
+  </div>
+  `;
+  $("#detail_film").html(html);
+  console.log(items);
+}
+
+
+function eventClickResearchMovie(e) {
+  e.preventDefault();
+  movieResearchByUser = $("#film").val();
+  API_URL_RESEARCH_MOVIE = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieResearchByUser}&language=fr`;
+  ajaxRequest(API_URL_RESEARCH_MOVIE, displayResearchMovie);
+  console.log(movieResearchByUser);
+  console.log(API_URL_RESEARCH_MOVIE);
+}
+
+
+
+function eventClickSelectedMovie(e) {
+  e.preventDefault();
+  idMovieSelectedByUser = this.id 
+  console.log(idMovieSelectedByUser)
+  API_URL_MOVIE_SELECTED = `https://api.themoviedb.org/3/movie/${idMovieSelectedByUser}?api_key=${API_KEY}&language=fr`;
+  ajaxRequest(API_URL_MOVIE_SELECTED, displaySelectedMovie);
+}
+
+
+// Evenement au clic du bouton recherche
+$("#bouton_search").on("click", eventClickResearchMovie);
+
+
+
