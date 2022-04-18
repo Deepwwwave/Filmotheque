77113be68e32f2eb0366f63ec63536fc8f89afce
deepwwwave <deepwwwave@gmail.com>
2022-04-18 20:00:29 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //Une fois le DOM monté => Appel de la fonction de requête ajax pour l'affichage des film de la semaine
 $(() => {
   ajaxRequest(API_URL_MOVIES_WEEK, displayWeekMovies);
-  gestionDisplayOverviewWeekMovies();
+  // Ecouteur boutton recherche 'clic'
+  $("#bouton_search").click(eventClickResearchMovie);
 });
 
 
-// Ecouteur boutton recherche 'clic'
-$("#bouton_search").click(eventClickResearchMovie);
+
 
 
 
